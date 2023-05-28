8d2507b9a6709bb167b244fd118914b750a2417c
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-29 02:19:00 +0300

    removed unwanted functions

--- stack_operations.c
+++ stack_operations.c
@@ -1,31 +1,6 @@
 #include "monty.h"
 
 /**
- * pop - removes the top element of the stack.
- * ---------------------
- * Description: The pop opcode removes the top element of the stack.
- * @stack: Double pointer to the top of the stack
- * @line_number: Line number in the file
- */
-
-void pop(stack_t **stack, unsigned int line_number)
-{
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	stack_t *temp = *stack;
-	*stack = (*stack)->next;
-
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-
-	free(temp);
-}
-
-/**
  * push - pushes an element to the stack
  * ---------------------
  * Description: pushes an element to the stack
