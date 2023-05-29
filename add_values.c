c1561980d0b76bd64f0dfbc6f3f1be10a97ef91d
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-29 23:50:24 +0300

    betty style review

--- add_values.c
+++ add_values.c
@@ -1,34 +1,51 @@
 #include "monty.h"
 
+/**
+ * add - Adds the top two elements of the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number of the instruction in the file
+ */
 void add(stack_t **stack, unsigned int line_number)
 {
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-        }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-        (*stack)->next->n += (*stack)->n;
-        pop(stack, line_number);
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
 }
 
-
+/**
+ * nop - Does nothing
+ * @stack: Double pointer to the stack
+ * @line_number: Line number of the instruction in the file
+ */
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
 }
 
+/**
+ * is_number - Checks if a string represents a number
+ * @n: String to check
+ * Return: 1 if the string is a number, 0 otherwise
+ */
 int is_number(const char *n)
 {
 	int i = 0;
+
 	if (*n == '-')
-	i = 1;
+		i = 1;
+
 	for (; *(n + i) != '\0'; i++)
 	{
 		if (isdigit(*(n + i)) == 0)
-			return 0;
+			return (0);
 	}
 
-    	return (1);
+	return (1);
 }
+
--- instructions.c
+++ instructions.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 
-/* Function: execute_instruction
- * ---------------------
- * Executes the corresponding function for a given opcode.
- *
- * instructions: Array of instruction_t structures containing opcode and function pairs
- * opcode: Opcode to execute
- * stack: Double pointer to the top of the stack
- * line_number: Line number in the file
+/**
+ * execute_instruction - Executes the corresponding function for a given opcode.
+ * @instructions: Array of instruction_t structures 
+ * @opcode: Opcode to execute
+ * @stack: Double pointer to the top of the stack
+ * Description: Executes the corresponding function for a given opcode.
+ * @line_number: Line number in the file
  */
 
 void execute_instruction(instruction_t *instructions, char *opcode, stack_t **stack, unsigned int line_number)
 {
 	int i;
+
 	for (i = 0; instructions[i].opcode != NULL; i++)
 	{
 		if (strcmp(opcode, instructions[i].opcode) == 0)
