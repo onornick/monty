c9639eefad2c363b8ad5a3ec802c30fd82ecb981
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-29 13:49:55 +0300

    remove mixed declaration

--- pop.c
+++ pop.c
@@ -10,13 +10,14 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
+        stack_t *temp;
+
         if (*stack == NULL)
         {
                 fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
                 exit(EXIT_FAILURE);
         }
 
-        stack_t *temp;
         temp = *stack;
         *stack = (*stack)->next;
 
