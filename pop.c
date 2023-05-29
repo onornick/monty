e4c75d7b8344126b97df009c520f2223ffe1f686
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-29 13:36:00 +0300

    initial commit

---
+++ pop.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack.
+ * ---------------------
+ * Description: The pop opcode removes the top element of the stack.
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number in the file
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+        if (*stack == NULL)
+        {
+                fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        stack_t *temp = *stack;
+        *stack = (*stack)->next;
+
+        if (*stack != NULL)
+                (*stack)->prev = NULL;
+
+        free(temp);
+}
+
+
