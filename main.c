66ffd8ee22fd740bf31d57b9235a9ec2de07a416
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-05-29 14:38:52 +0300

    added instructions

--- main.c
+++ main.c
@@ -6,6 +6,11 @@ int main(int argc, char *argv[])
 	instruction_t instructions[] = {
                 {"push", push},
                 {"pall", pall},
+		{"pint", pint},
+        	{"pop", pop},
+        	{"swap", swap},
+        	{"add", add},
+       	 	{"nop", nop},
                 {NULL, NULL}};
 
 	FILE *file;
